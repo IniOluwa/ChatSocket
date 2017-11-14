db8641a3629d8aec68ab21e2f32a5150d7886db0
IniOluwa <inioluwafageyinbo@gmail.com>
2017-11-14 19:56:32 +0100

    Views Engine And Server Logs Set.

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 // Initilize Dependencies
 const
-  app = require('express')(),
+  express = require('express'),
+  app = express();
+  router = express.Router();
   http = require('http').Server(app);
+  path = require('path'),
+  bodyParser = require('body-parser');
+  logger = require('morgan');
+
+
+// View Engine & Views
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+
+// Static Files
+app.use(express.static(__dirname + '/public'));
+
+// Server Port
+app.set('port', 3000 || process.env.PORT);
+
+// Body Parser To Enable Getting Post Data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Server Logs
+app.use(logger('dev'));
+
+// Landing
+app.get('/', (req, res) => {
+  res.render('login.html');
+});
+
+// Server Listening
+app.listen(app.get('port'), () => {
+  console.log("Server Listening At Port " + app.get('port'));
+});
